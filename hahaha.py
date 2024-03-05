a8d09d64e19a64ad06dccc631fd903951aeb25ef
vul1688 <vul1688@gmail.com>
2024-03-05 09:15:20 +0800

    add hehehe

--- hahaha.py
+++ hahaha.py
@@ -4,17 +4,17 @@ from starlette.requests import Request
 import ray
 from ray import serve
 
-
+import sklearn
 
 
 @serve.deployment(num_replicas=2, ray_actor_options={"num_cpus": 0.2, "num_gpus": 0})
 class Translator:    
     def translate(self, text: str) -> str:
-        return text+' hahaha'
+        return text+' hahaha' + sklearn.__version__
 
     async def __call__(self, http_request: Request) -> str:
         english_text: str = await http_request.json()
         return self.translate(english_text)
 
 
-translator_app = Translator.bind()
+hahaha_app = Translator.bind()
