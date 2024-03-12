995a51e719bb0f850d53bfe421f29095bfe4c587
vul1688 <vul1688@gmail.com>
2024-03-12 09:21:37 +0800

    commit

--- hehehe.py
+++ hehehe.py
@@ -1,4 +1,5 @@
 # File name: serve_quickstart.py
+import time
 from starlette.requests import Request
 
 import ray
@@ -10,6 +11,7 @@ import sklearn
 @serve.deployment(num_replicas=2, ray_actor_options={"num_cpus": 0.2, "num_gpus": 0})
 class Translator:    
     def translate(self, text: str) -> str:
+        time.sleep(1)
         return text+' hehehe' + sklearn.__version__
 
     async def __call__(self, http_request: Request) -> str:
--- model_client.py
+++ model_client.py
@@ -3,7 +3,12 @@ import requests
 
 english_text = "Hello world!"
 
-response = requests.post("http://127.0.0.1:8000/", json=english_text)
+response = requests.post("http://127.0.0.1:8000/hahaha", json=english_text)
+french_text = response.text
+
+print(french_text)
+
+response = requests.post("http://127.0.0.1:8000/hehehe", json=english_text)
 french_text = response.text
 
 print(french_text)
